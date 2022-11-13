d3d6aa4b7c47f6c436b502bd54ddeae52d564616
SantiagoMoreno-UdeA <santiago.moreno3@udea.edu.co>
2022-11-13 18:08:25 -0500

    Default values in Radio, default info modified

--- src/graph/GUI.py
+++ src/graph/GUI.py
@@ -4,6 +4,7 @@ Created on Fri Nov 11 16:01:08 2022
 
 @author: Santiago Moreno
 """
+
 import os 
 import gradio as gr
 import sys
@@ -46,77 +47,58 @@ def Tagger_sentence(Model, Sentence, Cuda):
 def Tagger_json(Model, Input_file, Output_file, Cuda):
     results = use_model(Model, Input_file, Output_file, Cuda)
     if type(results)==int:
-        return "Error {}, see documentation".format(results)
+        error_dict = {}
+        return "Error {}, see documentation".format(results), error_dict
     else:
         return { "text" : results['text'], 'entities': results['entities']}, results
 
 
 #---------------------------------GUI-------------------------------------
-with gr.Blocks() as demo:
-    gr.Markdown("Named Entity Recognition(NER) System. Use Tagger to do NER from a pretrained model, or use Trainer to train a new NER model")
-    with gr.Tab("Tagger"):
-        with gr.Tab("Sentence"):
+if __name__ == '__main__':
+    with gr.Blocks() as demo:
+        gr.Markdown("Named Entity Recognition(NER) System. Use Tagger to do NER from a pretrained model, or use Trainer to train a new NER model")
+        with gr.Tab("Tagger"):
+            with gr.Tab("Sentence"):
+                with gr.Row():
+                    with gr.Column():
+                        inputs =[
+                             gr.Radio(list(models), label='Model'),
+                             gr.Textbox(placeholder="Enter sentence here...", label='Sentence'), 
+                             gr.Radio([True,False], label='CUDA', value=False),
+                        ]
+                    output = gr.HighlightedText()
+                tagger_sen = gr.Button("Tag")
+                tagger_sen.click(Tagger_sentence, inputs=inputs, outputs=output)
+            with gr.Tab(".JSON"):
+                with gr.Row():
+                    with gr.Column():
+                        inputs =[
+                             gr.Radio(list(models), label='Model'),
+                             gr.Textbox(placeholder="Enter path here...", label='Input data file path'), 
+                             gr.Textbox(placeholder="Enter path here...", label='Output data file path'), #value='../../data/Tagged/document_tagged.json'),
+                             gr.Radio([True,False], label='CUDA', value=False),
+                        ]
+                    output = [
+                        gr.HighlightedText(),
+                        gr.JSON(),
+                        ]
+                tagger_json = gr.Button("Tag")
+                tagger_json.click(Tagger_json, inputs=inputs, outputs=output)
+        with gr.Tab("Trainer"):
             with gr.Row():
                 with gr.Column():
-                    inputs =[
-                         gr.Radio(list(models), label='Model'),
-                         gr.Textbox(placeholder="Enter sentence here...", label='Sentence'), 
-                         gr.Radio([True,False], label='CUDA'),
+                    train_input = inputs =[
+                         gr.Textbox(placeholder="Enter model name here...", label='New model name'),
+                         gr.Textbox(placeholder="Enter path here...", label='Input data directory path'), 
+                         gr.Radio([True,False], label='Upsampling', value=False),
+                         gr.Radio([True,False], label='CUDA', value=False),
                     ]
-                output = gr.HighlightedText()
-            tagger_sen = gr.Button("Tag")
-            tagger_sen.click(Tagger_sentence, inputs=inputs, outputs=output)
-        with gr.Tab(".JSON"):
-            with gr.Row():
-                with gr.Column():
-                    inputs =[
-                         gr.Radio(list(models), label='Model'),
-                         gr.Textbox(placeholder="Enter path here...", label='Input data file path'), 
-                         gr.Textbox(placeholder="Enter path here...", label='Output data file path'),
-                         gr.Radio([True,False], label='CUDA'),
-                    ]
-                output = [
-                    gr.HighlightedText(),
-                    gr.JSON(),
-                    ]
-            tagger_json = gr.Button("Tag")
-            tagger_json.click(Tagger_json, inputs=inputs, outputs=output)
-    with gr.Tab("Trainer"):
-        with gr.Row():
-            with gr.Column():
-                train_input = inputs =[
-                     gr.Textbox(placeholder="Enter model name here...", label='New model name'),
-                     gr.Textbox(placeholder="Enter path here...", label='Input data directory path'), 
-                     gr.Radio([True,False], label='Upsampling'),
-                     gr.Radio([True,False], label='CUDA'),
-                ]
-            train_output = gr.TextArea(placeholder="Output information", label='Output')
-        trainer = gr.Button("Train")
-
-
-    
-    trainer.click(Trainer, inputs=train_input, outputs=train_output)
+                train_output = gr.TextArea(placeholder="Output information", label='Output')
+            trainer = gr.Button("Train")
     
     
-# demo = gr.Interface(
-#     Tagging,
-#     [
-     
-#          gr.Radio(['Sentence', '.JSON']),
-#          gr.Radio(list(models)),
-#          gr.Textbox(placeholder="Enter sentence here..."), 
-#          gr.Textbox(placeholder="Enter sentence here..."), 
-#          gr.Textbox(placeholder="Enter sentence here..."),
-#          gr.Radio([True,False]),
-#     ],
-#     gr.HighlightedText(),
-#     # examples=[
-#     #     [5, "add", 3],
-#     #     [4, "divide", 2],
-#     #     [-4, "multiply", 2.5],
-#     #     [0, "subtract", 1.2],
-#     # ],
-#     title="Named Entity Recognizer",
-#     description="Named Entity Recognition system from a pretrained model",
-# )
-demo.launch(inbrowser=True)
+        
+        trainer.click(Trainer, inputs=train_input, outputs=train_output)
+        
+    
+    demo.launch(inbrowser=True)
